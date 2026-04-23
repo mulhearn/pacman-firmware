5a710ff44f0da95a31158098c5254edfb345173c
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2026-04-23 01:06:10 -0700

    bump firmware version to 3.6

--- vivado/src/hdl/firmware_version.vhd
+++ vivado/src/hdl/firmware_version.vhd
@@ -4,6 +4,6 @@ use ieee.numeric_std.all;
 
 package firmware_version is
   constant C_FIRMWARE_MAJOR  : integer  := 3;
-  constant C_FIRMWARE_MINOR  : integer  := 5;
+  constant C_FIRMWARE_MINOR  : integer  := 6;
   constant C_FIRMWARE_PATCH  : integer  := 0;
 end package firmware_version;
